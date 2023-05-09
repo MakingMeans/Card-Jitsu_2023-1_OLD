8fdb0ee555a386f25b3cb53ef6cf177ea8292720
Akalienjoyer <watchfiullfox@gamil.com>
2023-05-09 09:40:33 -0500

    ewiu

--- CardJitsu/src/co/edu/unbosque/controller/AplMain.java
+++ CardJitsu/src/co/edu/unbosque/controller/AplMain.java
@@ -1,10 +1,13 @@
 package co.edu.unbosque.controller;
+import java.util.Scanner;
 
 public class AplMain {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		Scanner sc = new Scanner(System.in);
+		
 	}
 
 }
